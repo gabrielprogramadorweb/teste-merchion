bf652a5bb3b2e05690eac33302f0c39666e3de0c
Gabriel Castro <gabriel.programador.ads@gmail.com>
2025-06-30 23:44:00 -0300

    Melhoria em ux e ui de aplicação, harmonizando as cores, deixando no tema proposto, Azul Turquesa

--- sistema/frontend/src/components/NavBar/NavBarView.vue
+++ sistema/frontend/src/components/NavBar/NavBarView.vue
@@ -7,18 +7,18 @@
             </router-link>
 
             <button class="navbar-toggler text-light border-0" type="button" @click="toggleMenu" aria-label="Toggle navigation">
-                <i v-if="!menuAberto" class="bi bi-list fs-3"></i>
-                <i v-else class="bi bi-x-lg fs-3"></i>
+                <i v-if="!menuAberto" style="color:#0dc9ee;" class="bi bi-list fs-3"></i>
+                <i v-else style="color:#0dc9ee;" class="bi bi-x-lg fs-3"></i>
             </button>
 
             <div class="collapse navbar-collapse justify-content-end" :class="{ show: menuAberto }" id="navbarContent">
                 <div class="d-flex flex-column flex-lg-row align-items-start align-items-lg-center gap-3 mt-3 mt-lg-0">
                     <template v-if="!userName">
-                        <router-link to="/login" class="btn btn-outline-light px-3 py-2 fw-semibold w-100 w-lg-auto">
+                        <router-link to="/login" class="btn btn-outline-light px-3 py-2 fw-semibold w-100 w-lg-auto" style="border: 1px solid #0dc9ee; color:#0dc9ee;">
                             <i class="bi bi-box-arrow-in-right me-1"></i> Entrar
                         </router-link>
                         <router-link
-                            to="/register" class="btn btn-outline-light text-white px-3 py-2 w-100 d-flex align-items-center justify-content-center" style="--bs-btn-hover-color: #fff; --bs-btn-hover-bg: transparent; --bs-btn-hover-border-color: #fff;">
+                            to="/register" class="btn btn-outline-light px-3 py-2 w-100 d-flex align-items-center justify-content-center" style="border: 1px solid #0dc9ee; color:#0dc9ee;--bs-btn-hover-color: #fff; hover-border-color: #fff;">
                             <i class="bi bi-person-plus-fill me-2"></i>
                             Registrar
                         </router-link>
