6cc4a3de0609d57dbab24eed65ec953fd1bbdc18
Gabriel Castro <gabriel.programador.ads@gmail.com>
2025-07-01 00:06:00 -0300

    Melhoria em tela Home, aplicando tema Azul Turquesa

--- sistema/frontend/src/pages/Home/HomeView.vue
+++ sistema/frontend/src/pages/Home/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="container d-flex flex-column justify-content-center align-items-center min-vh-100 text-center">
-        <div class="card shadow p-4" style="max-width: 500px; width: 100%;">
+        <div class="card shadow p-4" style="max-width:500px;width:100%;background-color:transparent;border: 1px solid #0dc9ee;">
             <div class="mb-3">
-                <i class="bi bi-person-check-fill display-4 text-primary"></i>
+                <i class="bi bi-person-check-fill display-4" style="color:#0dc9ee;"></i>
             </div>
-            <h1 class="h3 fw-bold">Bem-vindo ao Dashboard</h1>
-            <p class="text-muted">Você está autenticado com sucesso!</p>
+            <h1 class="h3 fw-bold text-white">Olá, Seja Bem vindo!</h1>
+            <p class="text-white">Você está autenticado com sucesso!</p>
             <router-link to="/tasks">
-                <button class="btn btn-primary mt-3">Ir para área Kanban</button>
+                <button class="btn btn-info mt-3 px-4 py-2 fw-semibold shadow-sm" style="color:#111827;">
+                    Ir para área Kanban
+                </button>
+
             </router-link>
 
         </div>
@@ -15,4 +18,6 @@
 </template>
 
 <script lang="ts" setup>
+</script>
+<script setup lang="ts">
 </script>
