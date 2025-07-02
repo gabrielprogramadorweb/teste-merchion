8a9f6b700ee87418a4ec081544fd928723ad81e9
Gabriel Castro <gabriel.programador.ads@gmail.com>
2025-07-01 22:03:06 -0300

    Ajuste em seed, alterando credencias de acordo com exigencias do form

--- sistema/database/seeders/UserSeeder.php
+++ sistema/database/seeders/UserSeeder.php
@@ -13,19 +13,19 @@
             User::create([
                 'name' => 'Usuario 01',
                 'email' => 'usuario01@flowtask.com',
-                'password' => Hash::make('123'),
+                'password' => Hash::make('@Flowtask123'),
             ]);
 
             User::create([
                 'name' => 'Usuario 02',
                 'email' => 'usuario02@flowtask.com',
-                'password' => Hash::make('123'),
+                'password' => Hash::make('@Flowtask123'),
             ]);
 
             User::create([
                 'name' => 'Admin',
                 'email' => 'admin@flowtask.com',
-                'password' => Hash::make('123'),
+                'password' => Hash::make('@Flowtask123'),
             ]);
         }
     }
